811c06869d6bb3f91e30da3dd5e4840007b550a3
stereopasa <stereopasa@gmail.com>
2023-06-01 23:41:31 +0300

    chore: redirect to https

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import './style.css';
+
+if (typeof window !== 'undefined') {
+  const { hostname, protocol } = window.location;
+  if (protocol === 'http:' && !/^(localhost|127.0.0.1)/.test(hostname)) {
+    window.location.protocol = 'https:';
+  }
+}
+
 // import typescriptLogo from "./typescript.svg";
 // import viteLogo from "/vite.svg";
 // import { setupCounter } from "./counter.ts";
