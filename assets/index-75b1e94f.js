06e81d3d45e0fc516e97509f443c118f1d618f83
stereopasa <pavel.smoliar@gmail.com>
2023-06-01 20:54:03 +0000

    Deploying to gh-pages from @ stereopasa/stereopasa.github.io@e6f4e6ede9c747ab544fa1073a496a1f5bfc63e5 🚀

---
+++ assets/index-75b1e94f.js
@@ -0,0 +1,5 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();if(typeof window<"u"){const{hostname:i,protocol:o}=window.location;o==="http:"&&!/^(localhost|127.0.0.1)/.test(i)&&(window.location.protocol="https:")}document.querySelector("#app").innerHTML=`
+  <div>
+    <h1>heya</h1>
+  </div>
+`;
